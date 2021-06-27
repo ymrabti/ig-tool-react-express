90e9e50b4873a1687d8ee7384cf53dde0b8a660a
ymrabti <47449165+ymrabti@users.noreply.github.com>
2021-06-27 15:22:03 +0100

    Fixing To deploy3

--- thebackend/src/models/database.js
+++ thebackend/src/models/database.js
@@ -6,7 +6,8 @@ require('mandatoryenv').load([
 const {
     DB_URL
 } = process.env;
-
+// mongodb://127.0.0.1:27017/IGTOOL
+// mongodb+srv://ymrabti:4CVUsBRBaIFJt5EZ@cluster0.guzvv.mongodb.net/IGTOOL?retryWrites=true&w=majority
 
 const { MongoClient, ObjectId} = require("mongodb");
 let conn = new MongoClient(DB_URL, {useUnifiedTopology: true});
